816185285f7151a72335fc06c5038eb5bdaeabe6
Arjun S <arjun.mi1@iiitmk.ac.in>
2016-12-05 15:13:15 +0530

    ReRan Classification from Start. Recreated GroundTruth

---
+++ USPS_DATASET/precision.py
@@ -0,0 +1,26 @@
+import sys
+Truth = sys.argv[1]
+Prediction = sys.argv[2]
+
+with open(Truth,'r') as gt:
+    GroundTruth = [int (i) for i in gt.readlines()] #readlines gives one line at a time.
+with open(Prediction,'r') as op:
+    Output = [int (i) for i in op.readlines()]
+
+def prec_recall(GroundTruth,uspsOutput):
+    tp = sum(1 for i, j in zip(GroundTruth, uspsOutput) if i == j == 1)
+    precision = tp / GroundTruth.count(1)
+    fn = sum(1 for i, j in zip(GroundTruth, uspsOutput) if (i == 1) and (j != 1))
+    recall = tp / (tp + fn)
+    return precision, recall
+
+print('{} instances in files. {} unique labels'.format(len(GroundTruth), len(set(GroundTruth))))
+
+for label in set(GroundTruth):
+    # A one-vs-all scheme
+    new_GroundTruth = [(1 if i == label else -1) for i in GroundTruth]
+    new_Output = [(1 if i == label else -1) for i in Output]
+
+    # calculate precision recall
+    precision, recall = prec_recall(new_GroundTruth, new_Output)
+    print("Class {}: Precision = {}, Recall = {}".format(label, precision, recall))
